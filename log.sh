ade74bb0f8da6ebac5ea76ebd8dbbaf1abce753a
h173309 <35480503+h173309@users.noreply.github.com>
2019-08-18 16:53:06 +0900

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,24 +1,33 @@
 #!/bin/sh
 cd
 
-# dotfiles
-git clone https://github.com/h173309/dotfiles.git
-cd dotfiles
-./install.sh
+sudo apt update
 
 # install linuxbrew
-sudo apt install ruby
-git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
-mkdir ~/.linuxbrew/bin
-ln -s ../Homebrew/bin/brew ~/.linuxbrew/bin
-eval $(~/.linuxbrew/bin/brew shellenv)
+# https://docs.brew.sh/Homebrew-on-Linux
+sudo apt install build-essential
 
-echo -e 'export PATH=${HOME}/.linuxbrew/bin:$PATH' >> ~/.bashrc
-source ~/.bashrc
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+brew install hello
+
+# git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
+# mkdir ~/.linuxbrew/bin
+# ln -s ../Homebrew/bin/brew ~/.linuxbrew/bin
+# eval $(~/.linuxbrew/bin/brew shellenv)
+
+# echo -e 'export PATH=${HOME}/.linuxbrew/bin:$PATH' >> ~/.bashrc
+# source ~/.bashrc
 
 # install zsh
 brew install zsh
-curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+which zsh | sudo tee -a /etc/shells
+chzh -s `which zsh`
 
-chzh
+# dotfiles
+git clone https://github.com/h173309/dotfiles.git
+cd dotfiles
+sh install.sh
